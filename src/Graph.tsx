61152080eccfaf7bbb549013dec99f59e55899cf
Catherine <wuyaocatherine@gmail.com>
2024-09-21 21:21:17 +0100

    Made necessary changes

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -14,7 +14,7 @@ interface IProps {
  * Perspective library adds load to HTMLElement prototype.
  * This interface acts as a wrapper for Typescript compiler.
  */
-interface PerspectiveViewerElement {
+interface PerspectiveViewerElement extends HTMLElement {
   load: (table: Table) => void,
 }
 
@@ -32,7 +32,7 @@ class Graph extends Component<IProps, {}> {
 
   componentDidMount() {
     // Get element to attach the table from the DOM.
-    const elem: PerspectiveViewerElement = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+    const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
     const schema = {
       stock: 'string',
@@ -49,8 +49,22 @@ class Graph extends Component<IProps, {}> {
 
       // Add more Perspective configurations here.
       elem.load(this.table);
+      // view = type of graph, y_line = continuous line graph
+      elem.setAttribute('view', 'y_line');
+      // column-pivots = allows us to distinguish stock ABC and DEF, stock = the value
+      elem.setAttribute ('column-pivots', '["stock"]')
+      // row-pivots = allows us to make the datapoint based on its timestamp
+      elem.setAttribute ('row-pivots', '["timestamp"]')
+      // columns  = focus on the stock data along y-axis (top_ask_price)
+      elem.setAttribute ('columns', '["top_ask_price"]')
+      // aggregates = handle duplicated dat by combining it into a single data point by averaging 
+      elem.setAttribute ('aggregates', `
+        {"stock": "distinct count",
+          "top_ask_price": "avg",
+          "top_bid_price": "avg",
+          "timestamp": "distinct count"} `);
+      }
     }
-  }
 
   componentDidUpdate() {
     // Everytime the data props is updated, insert the data into Perspective table
